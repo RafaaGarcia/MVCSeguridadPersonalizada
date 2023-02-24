ab1a77cb560240c9c17a30ac558f738bd949fea0
RafaaGarcia <rafa.gn@hotmail.com>
2023-02-24 18:01:31 +0100

    Add project files.

---
+++ MVCSeguridadPersonalizada/Controllers/ManagerControlelr.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MVCSeguridadPersonalizada.Controllers
+{
+    public class ManagerControlelr : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            return View();
+        }
+        [HttpPost]
+        Login(string username, string password)
+        {
+            if (username.ToLower() == "admin" && password.ToLower() == "admin")
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                Claim userName = new Claim(ClaimTypes.Name, username);
+                Claim role = new Claim(ClaimTypes.Role, "USUARIO");
+                identity.AddClaim(userName);
+                identity.AddClaim(role);
+                ClaimsPrincipal userPrincipal =
+                    new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync
+                    (CookieAuthenticationDefaults.AuthenticationScheme
+                    , userPrincipal
+                    , new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.Now.AddMinutes(15)
+                    });
+            }
+        }
+    }
+}
---
+++ MVCSeguridadPersonalizada/Filters/AuthorizeUsersAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MVCSeguridadPersonalizada.Filters
+{
+    public class AuthorizeUsersAttribute:AuthorizeAttribute,IAuthorizationFilter     
+    {
+        public void OnAuthorization(AutorizationFilterContext context)
+        {
+            var user =context.HttpContext.User;
+            if (user.identity.IsAutheticated == false)
+            {
+                RouteValueDictionary rutalogin =
+                    new RouteValueDictionary(new
+                    {
+                        Controller = "Manager",
+                        Action = "Login"
+                    });
+                context.Result = new RedirectToRouteResult(rutalogin);
+            }
+        }
+    }
+}
